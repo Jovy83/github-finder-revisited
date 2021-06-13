a6c0917dbb02992d2b855c80e8a9521b45a4794e
Jovy83 <jovy83@live.com>
2021-06-13 11:45:33 +0800

    implement search functionality via passing props up

--- src/App.js
+++ src/App.js
@@ -11,26 +11,39 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
+  // this will load github users upon page load
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   // Promise syntax
+  //   // axios.get('https://api.github.com/users').then(res => console.log(res.data))
+
+  //   // Async/await syntax (better)
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search Github users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
 
-    // Promise syntax
-    // axios.get('https://api.github.com/users').then(res => console.log(res.data))
-
-    // Async/await syntax (better)
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ users: res.data, loading: false });
-  }
+    // this endpoint gives the results in res.data.items
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} />
 
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
