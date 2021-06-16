dd2a439a07849ae75647b02c31c3975473534631
Jovy83 <jovy83@live.com>
2021-06-16 21:54:09 +0800

    create Home and NotFound components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-
+import Home from './components/pages/Home';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 
 import './App.css';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
+import NotFound from './components/pages/NotFound';
 
 const App = () => {
   // this will load github users upon page load
@@ -37,21 +36,13 @@ const App = () => {
               <Alert />
 
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <>
-                      <Search />
-
-                      <Users />
-                    </>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
 
                 <Route exact path='/about' component={About} />
 
                 <Route exact path='/user/:login' component={User} />
+
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
